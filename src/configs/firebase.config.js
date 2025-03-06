846deb6ade71bf5b47bc17ebb52a036e4302ab16
david <daviddelacruzmorales42@gmail.com>
2025-03-06 01:21:33 -0600

    updated

--- src/configs/firebase.config.js
+++ src/configs/firebase.config.js
@@ -1,6 +1,6 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("../../"+process.env.FIREBASE_KEY);
+var serviceAccount = require("../../firebase-adminsdk.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
