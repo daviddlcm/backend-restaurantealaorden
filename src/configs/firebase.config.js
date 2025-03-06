b3341b5c745ceedc76f79856ceeb64d6d97c1651
david <daviddelacruzmorales42@gmail.com>
2025-03-06 01:26:39 -0600

    updated

--- src/configs/firebase.config.js
+++ src/configs/firebase.config.js
@@ -1,7 +1,12 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("../../firebase-adminsdk.json");
+var serviceAccount = process.env.GOOGLE_APPLICATION_CREDENTIALS
+
+if (!serviceAccount) {
+  console.error('ERROR: No se encontró la variable GOOGLE_APPLICATION_CREDENTIALS');
+  process.exit(1);
+}
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(require(serviceAccount))
 });
